03f0cd9965bb135ff90dc39640e37f5ba0e3d714
Kyle Beard <73833102+kyle-beard-wex@users.noreply.github.com>
2023-07-07 10:29:22 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -111,36 +111,16 @@ extension Product {
 }
 
 extension Target {
-//    static let binarySource = BinarySource()
+    static let binarySource = BinarySource()
 
     static func binaryTarget(name: String, remoteChecksum: String) -> Target {
-        return .binaryTarget(
-            name: name,
-            path: localBinaryPath(for: name)
-        )
-//        switch binarySource {
-//        case .local:
-//            return .binaryTarget(
-//                name: name,
-//                path: localBinaryPath(for: name)
-//            )
-//        case .remote:
-//            return .binaryTarget(
-//                name: name,
-//                url: remoteBinaryURLString(for: name),
-//                checksum: remoteChecksum
-//            )
-//        }
-    }
-
-    static func localBinaryPath(for targetName: String) -> String {
-        "build/XCFrameworks/Static/\(targetName).xcframework"
+        return .binaryTarget(name: name, url: remoteBinaryURLString(for: name), checksum: remoteChecksum)
     }
 
     static func remoteBinaryURLString(for targetName: String) -> String {
         "https://github.com/facebook/facebook-ios-sdk/releases/download/v16.1.2/\(targetName)-Static_XCFramework.zip"
     }
-
+    
     static let aem = target(name: .aem, dependencies: [.Prefixed.aem])
 
     static let core = target(
@@ -159,36 +139,35 @@ extension Target {
 
     static let gaming = target(name: .gaming, dependencies: [.Prefixed.gaming])
 
-    /// NOTE: The checksum on an M1 machine differs from Intel, therefore our local SPM vs. Azure's SPM will not be the same...
     enum Prefixed {
         static let basics = binaryTarget(
             name: .Prefixed.basics,
-            remoteChecksum: "af64aad14fa8b0486ccb9f1483fe782e06acbe3309ff0c4143994bfc5ad62e6c"
+            remoteChecksum: binarySource == .local ? .LocalChecksum.basics : .RemoteChecksum.basics
         )
 
         static let aem = binaryTarget(
             name: .Prefixed.aem,
-            remoteChecksum: "d45388a46c0ac2e89c277136c6cc63137864b90022d2e785beb979ec09a8f70c"
+            remoteChecksum: binarySource == .local ? .LocalChecksum.aem : .RemoteChecksum.aem
         )
 
         static let core = binaryTarget(
             name: .Prefixed.core,
-            remoteChecksum: "2c5c1c3532f357d2a0e89035d8a41421709cacbe505cbc25880960d6f5907170"
+            remoteChecksum: binarySource == .local ? .LocalChecksum.core : .RemoteChecksum.core
         )
 
         static let login = binaryTarget(
             name: .Prefixed.login,
-            remoteChecksum: "9bd091a4accb23a143f6de71f0267279616287fa931138766723471242606bf8"
+            remoteChecksum: binarySource == .local ? .LocalChecksum.login : .RemoteChecksum.login
         )
 
         static let share = binaryTarget(
             name: .Prefixed.share,
-            remoteChecksum: "1dd3dd20ed42d651b992b9b1b71742c9e90630085f22f683d220bc64fca5321e"
+            remoteChecksum: binarySource == .local ? .LocalChecksum.share : .RemoteChecksum.share
         )
 
         static let gamingServices = binaryTarget(
             name: .Prefixed.gaming,
-            remoteChecksum: "fd6bb813cae23716afbc6f5f0c229bb5847b15bc3b6c50448b7b340b07213469"
+            remoteChecksum: binarySource == .local ? .LocalChecksum.gaming : .RemoteChecksum.gaming
         )
     }
 }
@@ -213,17 +192,13 @@ extension LinkerSetting {
     static let accelerateFramework = linkedFramework("Accelerate")
 }
 
-//enum BinarySource {
-//    case local, remote
-//
-//    init() {
-//        if getenv("USE_LOCAL_FB_BINARIES") != nil {
-//            self = .local
-//        } else {
-//            self = .remote
-//        }
-//    }
-//}
+enum BinarySource {
+    case local, remote
+
+    init() {
+        self = getenv("IGNORE_WEX_CHECKSUM") != nil ? .remote : .local
+    }
+}
 
 extension String {
     static let aem = "FacebookAEM"
@@ -240,5 +215,25 @@ extension String {
         static let login = "FBSDKLoginKit"
         static let share = "FBSDKShareKit"
         static let gaming = "FBSDKGamingServicesKit"
+    }
+    
+    /// This checksum corresponds to the WEX Netskope fuckery that occurs when they unzip and rezip files -_-
+    enum LocalChecksum {
+        static let aem = "d45388a46c0ac2e89c277136c6cc63137864b90022d2e785beb979ec09a8f70c"
+        static let basics = "af64aad14fa8b0486ccb9f1483fe782e06acbe3309ff0c4143994bfc5ad62e6c"
+        static let core = "2c5c1c3532f357d2a0e89035d8a41421709cacbe505cbc25880960d6f5907170"
+        static let login = "9bd091a4accb23a143f6de71f0267279616287fa931138766723471242606bf8"
+        static let share = "1dd3dd20ed42d651b992b9b1b71742c9e90630085f22f683d220bc64fca5321e"
+        static let gaming = "fd6bb813cae23716afbc6f5f0c229bb5847b15bc3b6c50448b7b340b07213469"
+    }
+    
+    /// This corresponds to the checksum that everyone else more fortunate to not encounter Netskope would see.
+    enum RemoteChecksum {
+        static let aem = "8e9e1ef0aadbbd3e140822b0fc3313b011bb959c7cdad5f1c1745dd4e96d8c4c"
+        static let basics = "ba2fab4ac759fcb4aba48c25f1222c0839c697d7cb7de4a5e28befe26b0b3caa"
+        static let core = "6fecfd2342d00a8020bc371b00b25cdd020acebed5ab20ef796c81d52fbcad22"
+        static let login = "d98bd590683281d5c8c3ea900047c3586126465d34c3dc264b8d51c01019a328"
+        static let share = "42d7dce3155e3c9c9a7db29789e3606757ad7b29a4f7ad9da77afbbe9bc41bd3"
+        static let gaming = "91f7433eadf3257cf0ff2a80e3364d52dd5b542c72876f6cdc1e1f7a6aec556f"
     }
 }
